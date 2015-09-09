203c9304c8b45af22bedca77979ba7eb53e56aea
mitsuru <mitsuru793@gmail.com>
2015-09-09 17:07:53 +0900

    add: ヒーローアビリティのyamlファイルのフォーマットチェックを行うテストファイル

---
+++ test/data/valid_heroes_ability_yaml_file_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+HEROES_NUMBER = 17
+describe 'number of heroers' do
+  it "is #{HEROES_NUMBER}"
+end
+
+describe 'all key name' do
+  it 'has no spaces'
+  it 'is_snake_case'
+  it 'does not start with'
+end
+
+describe 'value of weapon ratio' do
+  it 'is persent'
+end
+
+describe 'value of crystal ratio' do
+  it 'is persent'
+end
+
+describe 'passive ability' do
+  it 'is nil'
+end
+
+describe 'active ability' do
+  it 'is not nil'
+
+  describe '1 and 2' do
+    it 'has 5 values of effect'
+  end
+
+  describe '3' do
+    it 'has 3 values of effect'
+  end
+end
+
+describe 'each hero' do
+  it 'has 4 abilities'
+end
+
+describe 'each ability' do
+  it 'has cool down'
+  it 'has energy cost'
+end
