eab43cd8882353a5c9927ee3d337aefa20610624
mitsuru <mitsuru793@gmail.com>
2015-09-12 10:09:07 +0900

    add: ヒーローアビリティのyamlのkeyが,（カンマ）を含まないことをテスト

--- test/data/valid_heroes_ability_yaml_file_test.rb
+++ test/data/valid_heroes_ability_yaml_file_test.rb
@@ -28,6 +28,11 @@ describe 'all key name' do
       value.wont_match(/\?/)
     end
   end
+  it 'has no comma' do
+    ALL_KEYS.each do |value|
+      value.wont_match(/,/)
+    end
+  end
   it 'starts with lower case' do
     ALL_KEYS.each do |value|
       value.must_match(/^[a-z]/)
